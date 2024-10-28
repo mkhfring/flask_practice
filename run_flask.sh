0125a9859451e3ccabebad4198ad585c41b922d2
Mohamad Khajezade <12826803+mkhfring@users.noreply.github.com>
2024-10-27 22:18:39 -0700

    Adding a .sh file to run the applicaiton

---
+++ run_flask.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set environment variables
+export FLASK_APP=flasker  # Replace 'app.py' with the name of your main Flask file
+export FLASK_ENV=development  # Optional: Enables debug mode for development
+
+# Run the Flask application
+flask run
